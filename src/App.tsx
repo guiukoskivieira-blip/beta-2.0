8eaf312142b5a515e5b845f7570f28cd2841ef5b
Guilherme Vieira <Guiukoskivieira@gmail.com>
2026-08-23 19:34:06 -0300

    fix: include PDFX panel integration in app

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { VisualPreview } from './components/VisualPreview';
 import { FixEnginePanel } from './components/FixEnginePanel';
 import { TrimBleedFixPanel } from './components/TrimBleedFixPanel';
 import { ImageColorFixPanel } from './components/ImageColorFixPanel';
+import { PdfxPreparationPanel } from './components/PdfxPreparationPanel';
 
 import { STANDARD_PROFILES, COMMERCIAL_PRINT_300DPI_PROFILE, ProductionProfile } from './utils/productionProfiles';
 import { runDeterministicRuleEngine } from './utils/ruleEngine';
@@ -269,6 +270,7 @@ export const App: React.FC = () => {
                 <FixEnginePanel analysis={currentAnalysis} />
                 <TrimBleedFixPanel analysis={currentAnalysis} profile={selectedProfile} originalFile={selectedFile} />
                 <ImageColorFixPanel analysis={currentAnalysis} profile={selectedProfile} originalFile={selectedFile} />
+                <PdfxPreparationPanel analysis={currentAnalysis} profile={selectedProfile} originalFile={selectedFile} />
                 <DiagnosticPanel ruleResults={currentAnalysis.ruleResults} />
                 <AiAssistant analysis={currentAnalysis} />
               </div>
